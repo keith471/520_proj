87ff411f7e8bc9d01b4fbf915995da6c74bb92a6
Keith Strickling <keithstrickling@gmail.com>
2017-02-25 23:21:51 -0500

    updated prett.h

--- src/pretty.h
+++ src/pretty.h
@@ -4,5 +4,24 @@
 #include "tree.h"
 
 void prettyPROGRAM(PROGRAM *p, char* filePath);
+void prettyPACKAGE(PACKAGE* package);
+void prettyTOPLEVELDECLARATION(TOPLEVELDECLARATION* tld);
+void prettyVARDECLARATION(VARDECLARATION* vd, int level);
+void prettyVARDECLARATIONdistributedbody(VARDECLARATION* vd, int level);
+void prettyVARDECLARATIONsingleline(VARDECLARATION* vd);
+void prettyID(ID* id);
+void prettyTYPEDECLARATION(TYPEDECLARATION* td, int level);
+void prettyTYPEDECLARATIONdistributedbody(TYPEDECLARATION* td, int level);
+void prettyTYPEDECLARATIONsingle(TYPEDECLARATION* td, int level);
+void prettyTYPE(TYPE* type, int level);
+void prettyFIELD(FIELD* field, int level);
+void prettyFUNCTIONDECLARATION(FUNCTIONDECLARATION* fd);
+void prettySTATEMENT(STATEMENT* s, int level, int semicolon, int startAtRwPointer);
+void terminateSTATEMENT(int level, int semicolon);
+void prettySWITCHCASE(SWITCHCASE* s, int level);
+void prettyBinOp(OperationKind opKind);
+void prettyPARAMETER(PARAMETER* param);
+void prettyEXPs(EXP* exp);
+void prettyEXP(EXP* e);
 
 #endif /* !PRETTY_H */
