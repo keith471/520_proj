c264758c7117a86daa9074e37a475c64490ea306
Keith Strickling <keithstrickling@gmail.com>
2017-03-22 16:11:12 -0400

    did some work on codegen

--- TODO.md
+++ TODO.md
@@ -20,10 +20,31 @@ ssh kstric@teaching.cs.mcgill.ca
 
 
 ## Codegen
+- make a pass where we collect arrays and structs and make typedefs for them
+- any node where we encounter these arrays and structs, we will need to update the node
+with the new representative C++ array or struct type that we've made for it
+    ==> I think it would be very helpful to give each (TYPE) node a C++ type based on this traversal!
+    Rules:
+        - if the GoLite type either is or resolves to a base type, then the C++ type is the base type
+        - if the GoLite type either is or resolves to a slice type, then the C++ type is a vector<T> where T
+        is the type that the slice element types resolve to
+        - any time we see a struct or array (either a literal or a named type) we need to compare it with the structs and arrays that we've already encountered and saved as C++ types. If we find a matching one, then the C++ type of the current struct or array is the C++ type that we found.
+
+
+        - for structs and arrays, we can have either named structs or arrays (declared as type name struct {...} etc.) or unnamed literals (var x struct {...} etc.)
+            - we can create a linked list (top is the first struct or array we encounter) of C++ typedefs (if unnamed, then we give it a name, else it has the name given to it in GoLite)
+            - if the GoLite type is an idT that resolves to an array, then we need to look in the hashmap for the id and get the associated C++ array
+                - if we can't find it then we create a new one and add it to the hashmap
+            - if the GoLite type is an array literal, then we ad
+
 - genVARDECLARATION
 - genTYPEDECLARATION
 - genFUNCTIONDECLARATION
+- finish genTYPE ==> idK, structK, arrayK
+- finish genDefault
 - write test programs and report!
+
+- a c++ vector is just like a slice!!!
 
 ## Sources
 http://stackoverflow.com/questions/10826744/semicolon-insertion-ala-google-go-with-flex
--- src/codegen.c
+++ src/codegen.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include "tree.h"
 #include "codegen.h"
 #include "outputhelpers.h"
@@ -24,7 +25,10 @@ void genPROGRAM(PROGRAM* p, char* fname) {
     emitFILE = fopen(fname, "w");
     addHeaderCode();
     newLineInFile(emitFILE);
-    // traverse the AST, writing each line's equivalent C++ code to emitFILE
+    // TODO first, we need to make a pass that finds and aggregates structs and arrays
+    // and then makes typedefs for them at the top of the file
+
+    // next, traverse the AST, writing each line's equivalent C++ code to emitFILE
     // we completely ignore the package declaration
     genTOPLEVELDECLARATION(p->topLevelDeclaration);
     // close emitFILE
@@ -48,7 +52,42 @@ void genTOPLEVELDECLARATION(TOPLEVELDECLARATION* tld) {
 }
 
 void genVARDECLARATION(VARDECLARATION* vd, int level) {
+    if (vd == NULL) return;
+    genVARDECLARATIONlist(vd, level);
+    genVARDECLARATION(vd->nextDistributed, level);
+}
 
+/*
+ * starting at the current line, moves the rw-pointer level tabs in and prints
+ * a variable declaration there. Follows it with a semicolon and a new line
+ */
+void genVARDECLARATIONlist(VARDECLARATION* vd, int level) {
+    if (vd == NULL) return;
+    printTabsToFile(emitFILE, level);
+    switch (vd->kind) {
+        case typeOnlyK:
+            // need to have a function that assigns defaults depending on the type!
+            genTYPE(vd->val.typeVD);
+            fprintf(emitFILE, " %s = ", vd->id->name);
+            genDefault(vd->val.typeVD);
+            break;
+        case expOnlyK:
+            // get the type from the expression
+            genTYPE(vd->val.expVD.exp->type);
+            fprintf(emitFILE, " %s = ", vd->id->name);
+            genEXP(vd->val.expVD.exp);
+            break;
+        case typeAndExpK:
+            // easy --> you already have all the information you need
+            genTYPE(vd->val.typeAndExpVD.type);
+            fprintf(emitFILE, " %s = ", vd->id->name);
+            genEXP(vd->val.typeAndExpVD.exp);
+            break;
+    }
+    // finish it up with a semicolon and a new line
+    fprintf(emitFILE, ";");
+    newLineInFile(emitFILE);
+    genVARDECLARATIONlist(vd->next, level);
 }
 
 void genTYPEDECLARATION(TYPEDECLARATION* td, int level) {
@@ -56,13 +95,96 @@ void genTYPEDECLARATION(TYPEDECLARATION* td, int level) {
 }
 
 void genFUNCTIONDECLARATION(FUNCTIONDECLARATION* fd) {
-    /*
-     We need to recognize the main function! If there is a function called main that has no arguments
-     and either no return type or int return type, then this is main :)
-    */
+    // print the function signature
     if (isMain(fd)) {
-
+        fprintf(emitFILE, "int main() {");
+    } else {
+        if (fd->returnType == NULL) {
+            fprintf(emitFILE, "void ");
+        } else {
+            genTYPE(fd->returnTYPE);
+        }
+        fprintf(emitFILE, "%s(", fd->id->name);
+        genPARAMETER(fd->parameters);
+        fprintf(emitFILE, ") {");
     }
+    // print the function body
+    newLineInFile(emitFILE);
+    genSTATEMENT(fd->statements, 1);
+    // close off the function body
+    fprintf(emitFILE, "}");
+    newLineInFile(emitFILE);
+    newLineInFile(emitFILE);
+}
+
+void genPARAMETER(PARAMETER* p) {
+    if (p == NULL) return;
+    genPARAMETERlist(p);
+    if (p->nextParamSet != NULL) {
+        fprintf(emitFILE, " ,");
+        genPARAMETER(p->nextParamSet);
+    }
+}
+
+void genPARAMETERlist(PARAMETER* p) {
+    if (p == NULL) return;
+    genTYPE(p->type);
+    fprintf(emitFILE, " %s", p->id->name);
+    if (p->nextId != NULL) {
+        fprintf(emitFILE, ", ");
+        genPARAMETERlist(p->nextId);
+    }
+}
+
+/*
+ * outputs the type at the current read/write pointer
+ * does not print a new line after
+ */
+void genTYPE(TYPE* t) {
+    switch (t->kind) {
+        case intK:
+            fprintf(emitFILE, "int");
+            break;
+        case float64K:
+            fprintf(emitFILE, "double");
+            break;
+        case runeK:
+            fprintf(emitFILE, "char");
+            break;
+        case boolK:
+            fprintf(emitFILE, "bool");
+            break;
+        case stringK:
+            fprintf(emitFILE, "char*");
+            break;
+        case idK:
+            // TODO seems we need to use the underlying type here!!!
+            genTYPE(t->val.idT.underlyingType);
+            break;
+        case structK:
+            // TODO seems we need to perform a pass through the entire program first to collect structs
+            // so that we can declare them before the functions (determine where exactly to declare them!).
+            // Also, if two structs have identical fields, then it seems we just declare the struct once
+            // (though honestly we probably don't have to do this - declaring two structs with the same fields
+            // but different names is probably fine)
+            break;
+        case sliceK:
+            fprintf(emitFILE, "vector<");
+            genTYPE(t->val.sliceT);
+            fprintf(emitFILE, ">");
+            break;
+        case arrayK:
+            // TODO we need to traverse the program first and find any arrays and declare a type for them
+            break;
+    }
+}
+
+/*
+ * generates the expression in C++ at the current rw-pointer
+ * does not print a semicolon or new line after
+ */
+void genEXP(EXP* e) {
+
 }
 
 /*
@@ -70,5 +192,62 @@ void genFUNCTIONDECLARATION(FUNCTIONDECLARATION* fd) {
  * and 0 otherwise
  */
 int isMain(FUNCTIONDECLARATION* fd) {
+    // the name must be main
+    if (strcmp(fd->id->name, "main") != 0) {
+        return 0;
+    }
+    // there can be no parameters
+    if (fd->parameters != NULL) {
+        return 0;
+    }
+    // the return can only be void or int
+    if (fd->returnType != NULL) {
+        if (fd->returnType->kind != intK) {
+            return 0;
+        }
+    }
+    return 1;
+}
 
+/*
+ * generates the default value a variable is assigned to based on its type
+ * prints the value at the current rw-pointer; does not print a new line after
+ */
+void genDefault(TYPE* t) {
+    switch (t->kind) {
+        case intK:
+            fprintf(emitFILE, "0");
+            break;
+        case float64K:
+            fprintf(emitFILE, "0.0");
+            break;
+        case runeK:
+            fprintf(emitFILE, "0");
+            break;
+        case boolK:
+            fprintf(emitFILE, "0");
+            break;
+        case stringK:
+            fprintf(emitFILE, "\"\"");
+            break;
+        case idK:
+            // TODO seems we need to use the underlying type here!!!
+            genDefault(t->val.idT.underlyingType);
+            break;
+        case structK:
+            // TODO seems we need to perform a pass through the entire program first to collect structs
+            // so that we can declare them before the functions (determine where exactly to declare them!).
+            // Also, if two structs have identical fields, then it seems we just declare the struct once
+            // (though honestly we probably don't have to do this - declaring two structs with the same fields
+            // but different names is probably fine)
+            break;
+        case sliceK:
+            fprintf(emitFILE, "vector<");
+            genTYPE(t->val.sliceT);
+            fprintf(emitFILE, ">()");
+            break;
+        case arrayK:
+            // TODO we need to traverse the program first and find any arrays and declare a type for them
+            break;
+    }
 }
